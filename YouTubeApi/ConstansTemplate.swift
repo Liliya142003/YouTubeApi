342f06525f74d6b3f2af2d7e31b974b690455e99
Лілія Вельбой <liliya142003@gmail.com>
2023-02-21 19:03:44 +0200

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

---
+++ YouTubeApi/ConstansTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstansTemplate.swift
+//  YouTubeApi
+//
+//  Created by Лілія on 21.02.2023.
+//
+
+// Add your own keys below and unkoment the code
+import Foundation
+
+
+
+//struct Constants {
+//    static var apiKey = ""
+//    static var playlistId = ""
+//    static var apiUrl = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.playlistId)&key=\(Constants.apiKey)"
+//}
